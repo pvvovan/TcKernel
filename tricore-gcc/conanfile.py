643bca0172bbb76b63287f9da866b884ecb73953
PVVovan <pvvovan@gmail.com>
2026-09-20 16:16:24 +0200

    Download tricore-gcc archive to temp directory

--- tricore-gcc/conanfile.py
+++ tricore-gcc/conanfile.py
@@ -1,9 +1,37 @@
+import os
+import tempfile
+
 from conan import ConanFile
+from conan.tools.files import copy, download, unzip
+from urllib.parse import urljoin
+
+required_conan_version = ">=2.32"
 
 
 class TriCoreGccConan(ConanFile):
 	name = "tricore-gcc"
+	settings = "arch", "os"
 	version = "13.4.1-0"
+	zipfile = None
+
+	def validate(self):
+		if self.settings.arch != "x86_64":
+			raise ConanInvalidConfiguration("GCC binaries are only provided for x86_64")
+		if self.settings.os not in ["Linux", "Windows"]:
+			raise ConanInvalidConfiguration("GCC binaries are only provided for Windows and Linux")
 
 	def build(self):
-		pass
+		source_entry = self.conan_data["sources"]["v13.4.1"][str(self.settings.os)]
+		self.zipfile = os.path.join(tempfile.gettempdir(), "tricore-gcc-tempdir", source_entry["file"])
+		if not os.path.isfile(self.zipfile):
+			download(self,
+				url = urljoin(source_entry["url"], source_entry["file"]),
+				filename = self.zipfile,
+				sha256 = source_entry["sha256"])
+
+	def package(self):
+		unzip(self,
+			filename = self.zipfile,
+			destination = self.package_folder,
+			keep_permissions = True,
+			strip_root = True)
--- tricore-gcc/create.sh
+++ tricore-gcc/create.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 python3 -m venv "${SCRIPT_DIR}/pvenv"
 . "${SCRIPT_DIR}/pvenv/bin/activate"
-python3 -m pip install -r "${SCRIPT_DIR}/requirements.txt"
+python3 -m pip install -r "${SCRIPT_DIR}/../requirements.txt"
 conan profile detect > /dev/null 2>&1 || true
-conan build "${SCRIPT_DIR}" --output-folder "${SCRIPT_DIR}/lbuild"
+conan create "${SCRIPT_DIR}" --user tc --channel gcc
